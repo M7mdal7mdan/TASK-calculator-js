69e8fc4beabc3ec7098f6854638aee63eefa0841
Your Name <mohammadalhamdan94@gmail.com>
2022-01-03 16:07:20 +0300

    task done with my pair Ahamd Dashti

--- index.js
+++ index.js
@@ -7,10 +7,65 @@
  * If you click on button +, the text will be "+"
  * ,... and so on
  */
+
+ let number1 ;
+ let number2 ;
+ let result;
+ let operations;
+ let history = [];
+ 
+
 function buttonClick(text) {
-  console.log("Clicking", text);
+  
+  
   // Write your code here
+if (text =="+" || text =="-" || text == "÷" || text == "x"){
+  operations = text;
 }
+else if (text ==="AC"){
+  printOnConsole(0);
+  number1 = 0;
+  number2 = 0;
+}
+else if (!isNaN(text)){
+printOnConsole(text);
+  if (!number1) number1 = parseInt(text);
+  else if (operations) number2 = parseInt(text);
+}
+
+else if (text == "="){
+
+result = (calculate(operations,number1,number2));
+printOnConsole(result);
+history.push(`${number1} ${operations} ${number2 } = ${result}`);
+console.log(history);
+updateHistory(history);
+
+}
+
+
+
+
+}
+
+
+
+function calculate(operations, number1,number2){
+switch(operations){
+
+  case "+":
+    return number1 + number2;
+    case "-":
+    return number1 - number2;
+    case "x":
+    return number1 * number2;
+    case "÷":
+    return number1 / number2;
+}
+
+
+}
+
 
 /** Supporting functions
  * 1. `printOnConsole(text)`, takes any text, and renders the console part of the web page
@@ -18,5 +73,5 @@ function buttonClick(text) {
  */
 
 // Remove Me after testing
-printOnConsole("123");
-updateHistory(["This is a sample historry", "1 + 5 = 6", "5 x 10 = 50"]);
+//printOnConsole();
+
